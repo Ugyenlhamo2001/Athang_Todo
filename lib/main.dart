892e8374d8d6c05b243afe8dd1b26d1a64105254
ugyenlhamo <ugyenlhamo@dk.bt>
2024-05-17 16:35:37 +0600

    nevigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:stateful_project/Screens/Home.dart';
+import 'package:stateful_project/Screens/LoginTask.dart';
+import 'package:stateful_project/screens/AddTask.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,17 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Todohome(),
+      // home: Logintask(),
+      // home is no longer required when using named routes
+      // we must define the initial route for the app
+      // initial route defines which route to open when app start
+      initialRoute: '/',
+      // routes is an object that holds path which defines which screen
+      // to open when we navigated through name
+      routes: {
+        '/': (context) => Logintask(),
+        '/home': (context) => Addtask(),
+      },
     );
   }
 }
